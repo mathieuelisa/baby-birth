ba5c9f7c1b60dade222e8abfeced4962edaddf36
Mills <mathieu.elisa@gmail.com>
2026-04-11 21:23:22 +0200

    adding metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,31 @@ import { Providers } from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "babyBirth",
-  description: "babyBirth application",
+  title: "Mini Nous – Liste de naissance & pronostics bébé",
+  description:
+    "Mini Nous est une application dédiée a notre liste de naissance et aux pronostics de bébé.",
+
+  keywords: [
+    "liste de naissance",
+    "pronostics bébé",
+    "date naissance bébé",
+    "jeu naissance bébé",
+    "liste naissance en ligne",
+    "baby shower jeu",
+    "deviner naissance bébé",
+    "application naissance",
+    "organisation naissance",
+  ],
+
+  openGraph: {
+    title: "Mini Nous – Liste de naissance & pronostics bébé",
+    description:
+      "Liste de naissance et pronostics sur futur bébé : date, prénom, poids… Une expérience fun et collaborative.",
+    url: "https://baby-birth-neon.vercel.app/home",
+    siteName: "Mini Nous",
+    locale: "fr_FR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -16,7 +39,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="fr" suppressHydrationWarning>
+    <html lang='fr' suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
